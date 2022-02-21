214a3fef7916dd7860c9bd65c5c31e2dcdfaae57
Connie Bernardin <conniebernardin@googlemail.com>
2022-02-21 14:08:35 +0000

    add SQL file

--- src/main/java/com/up/libraryBookingSystem/dao/libraryDataBase.sql
+++ src/main/java/com/up/libraryBookingSystem/dao/libraryDataBase.sql
@@ -1,2 +1,38 @@
-DROP TABLE assignments;
-DROP TABLE animals;
+DROP TABLE books;
+DROP TABLE users;
+DROP TABLE authors;
+
+CREATE TABLE authors (
+    author_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    nationality VARCHAR(255),
+    image TEXT
+);
+
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    genre VARCHAR(255) NOT NULL,
+    author_id INT REFERENCES authors(author_id),
+    loaned BOOLEAN NOT NULL,
+    ISBN INT NOT NULL
+);
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    username VARCHAR (255) NOT NULL,
+    isManager BOOLEAN NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO authors (name, nationality) VALUES ('Jane Austen', 'English');
+
+INSERT INTO books (title, genre, author_id, loaned, ISBN) VALUES ('Emma', 'Classic', 1, FALSE, 1234);
+INSERT INTO books (title, genre, author_id, loaned, ISBN) VALUES ('Pride and Prejudice', 'Classic', 1, FALSE, 12345);
+
+INSERT INTO users (name, username, isManager, password) VALUES ('Yang', 'Yang', TRUE, 'password');
+INSERT INTO users (name, username, isManager, password) VALUES ('Wendy', 'WendyDiane', FALSE, 'password');
+INSERT INTO users (name, username, isManager, password) VALUES ('Michelle', 'Michelle', FALSE, 'password');
+INSERT INTO users (name, username, isManager, password) VALUES ('Connie', 'ConnieB', FALSE, 'password');
+
