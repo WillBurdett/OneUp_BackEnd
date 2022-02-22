15dbb2bba9c4f6c5fd515fcb6005f518957ef455
Connie Bernardin <conniebernardin@googlemail.com>
2022-02-22 12:46:57 +0000

    add business logic for user service

--- src/main/java/com/up/libraryBookingSystem/service/UserService.java
+++ src/main/java/com/up/libraryBookingSystem/service/UserService.java
@@ -1,4 +1,96 @@
 package com.up.libraryBookingSystem.service;
 
+import com.up.libraryBookingSystem.dao.UserDao;
+import com.up.libraryBookingSystem.pojo.Users;
+import org.apache.catalina.User;
+
+import java.util.List;
+
 public class UserService {
+
+    private UserDao usersDao;
+
+    public UserService(UserDao usersDao) {
+        this.usersDao = usersDao;
+    }
+
+    private boolean userExists(Integer userId) {
+        return usersDao
+                .selectAllUsers()
+                .stream()
+                .anyMatch(user -> user.getSerialID().equals(userId));
+    }
+
+    private boolean passwordSecure(String password) {
+
+        boolean hasLetter = false;
+        boolean hasDigit = false;
+
+        if (password.length() >= 8) {
+            for (int i = 0; i < password.length(); i++) {
+                char x = password.charAt(i);
+                if (Character.isLetter(x)) {
+                    hasLetter = true;
+                } else if (Character.isDigit(x)) {
+                    hasDigit = true;
+                }
+                // no need to check further, break the loop because it's true
+                if (hasLetter && hasDigit) {
+                    return true;
+                }
+            }
+            if (hasLetter && hasDigit) {
+            } else {
+                throw new IllegalStateException("PASSWORD NOT STRONG");
+            }
+        } else {
+            throw new IllegalStateException("PASSWORD NEEDS AT LEAST 8 CHARACTERS");
+        }
+        return false;
+    }
+
+    public void addUser(Users user) {
+        boolean exists = userExists(user.getSerialID());
+        boolean secure = passwordSecure(user.getPassword());
+        if (exists) {
+            throw new IllegalStateException("User already exists");
+        } else if (user.getName() == null) {
+            throw new IllegalStateException("No name entered.");
+        } else if (user.getUsername() == null) {
+            throw new IllegalStateException("No username entered");
+        } else if (user.getPassword() == null) {
+            throw new IllegalStateException("No password entered");
+        } else if (!secure) {
+            throw new IllegalStateException("Password not secure");
+        } else if (user.getManager()) {
+            throw new IllegalStateException("Manager already exists.");
+        } else {
+            usersDao.addUser(user);
+        }
+    }
+
+
+    public void deleteUser(Integer userId) {
+        boolean exists = userExists(userId);
+        if (!exists) {
+            throw new IllegalStateException("User does not exist");
+        } else {
+            usersDao.deleteUser(userId);
+        }
+    }
+
+    public void updateUser(Users user, Integer userId) {
+        boolean exists = userExists(userId);
+
+        int result = usersDao.updateUser(userId, user);
+
+        if (result != 1) { //if result isn't one then you know that something failed.
+            throw new IllegalStateException("Could not update user in database. Input not valid");
+        }
+    }
+
+    public List<Users> displayUsers() {
+        return usersDao.selectAllUsers();
+    }
+
 }
